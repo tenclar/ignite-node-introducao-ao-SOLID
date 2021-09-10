2a69eff345029928ea3ea8f58e0f5d610da63083
tenclar <tenclarvalus@gmail.com>
2021-09-09 21:49:39 -0500

    controllers

--- src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
+++ src/modules/users/useCases/listAllUsers/ListAllUsersController.ts
@@ -6,7 +6,9 @@ class ListAllUsersController {
   constructor(private listAllUsersUseCase: ListAllUsersUseCase) {}
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    const { user_id } = request.headers;
+    const list = this.listAllUsersUseCase.execute({ user_id: String(user_id) });
+    return response.json(list);
   }
 }
 
